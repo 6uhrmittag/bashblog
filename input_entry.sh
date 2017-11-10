fffb03e3d92726117208ba251d8d0894a999fa7a
Marvin <marvin@laptop.local>
2017-11-10 23:33:43 +0100

    Updated: Input routine
    added option to edit entry, cancel input
    Updated formatting

--- input_entry.sh
+++ input_entry.sh
@@ -7,48 +7,57 @@
 #Tags in html link ändern
 
 
-#loadad config
+#load config
 path_config="./config/config.conf"
 . $path_config
 
+#Initialize Variables
+confirm="e"
+url=""
+entry_text=""
+tags=""
+
+#Start input
+while [ "$confirm" = "e" ] ; do
+  printf "URL: "
+  read   url
+
+  printf "Text: "
+  read entry_text
+
+  printf "Tags: "
+  read tags
+
+  echo "----------------------------"
+  echo "Entry:"
+  echo "URL: $url"
+  echo "Text: $entry_text"
+  echo "Tags: $tags"
+  echo "----------------------------"
+
+  #printf "You entered: \n$url\n$entry_text\n$tags\n"
 
 
-#Variablen setzen
-confirm="n"
+  #Save entry
+  echo "Publish = ENTER"
+  echo "Edit    = e"
+  echo "Cancel  = c"
+  read confirm
+  if [ "$confirm" = "c" ]; then
+    break
+  elif [[ "$confirm" = "e" ]]; then
+    echo "next try"
+  else
+    #Create entry html
+    . $install_path/beitraggenerieren.sh
 
-#Eintragen ANFANG
-while [ "$confirm" = "n" ] ; do
-printf "URL: "
-read   url
-
-printf "Text: "
-read entry_text
-
-printf "Tags: "
-read tags
-
-printf "You entered: \n$url\n$entry_text\n$tags\n"
-
-
-#Abfrage ist noch fehlerhaft. bei "else" sollte der komplette untere Teil übersprungen werden, macht sonst keinen Sinn.
-printf "Save entry?(y/n)"
-read confirm
-if [ "$confirm" = "y" ]; then
+    #Generate new index.html:
+    . $path_scripts/generate_index.sh
 
 
-  #Create entry html
-  . $install_path/beitraggenerieren.sh
-
-
-#Neue Index.html erstellen:
-. $path_scripts/generate_index.sh
-
-
-  echo "Saved."
-else
-  echo "Canceled."
-  break
-fi
+    echo "Entry published!"
+    break
+  fi
 
 done
 #Eintragen ENDE
