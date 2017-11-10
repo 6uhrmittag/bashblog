1b95e9600eb538d1ed9e74de8465dae971299b13
Marvin <marvin@laptop.local>
2017-11-10 21:50:20 +0100

    file translated

--- mail.sh
+++ mail.sh
@@ -1,78 +1,67 @@
 #!/bin/bash
 #TODO: tags
 
+#Debugmessage
 now=$(date +"%T")
 echo "$now - Start Email import"
 
-#loadad config
+#load config
 path_config="./config/config.conf"
 . $path_config
 file_mailxml="$path_temp/mail.xml"
 file_ids="$path_ids/ids.txt"
 touch $file_ids
 
+#Initialize Variables
+counter_entries_imported=0
+counter_entry=1
+
+#Download RSS feed
 feed=$(curl --silent "https://zapier.com/engine/rss/2528358/blogeintrag/")
 feed=$(echo $feed | sed 's/%0D%0A%0D%0A//g' | sed 's/%0D%0A//g')
 echo $feed > $file_mailxml
 
-phasedfeed=$(xml_grep "/*" $file_mailxml)
-zahl=1
-beitagscounter=0
-#echo $phasedfeed
-
-#Einträge zählen
-#beitragscounter=$(xml_grep --count "rss//item/title" mail.xml | head -n 1 | cut -d: -f2 | tr -d '[:space:]')
-#echo "Beiträge gefunden: $beitragscounter"
-beitragscounter=$(xmlstarlet sel -t -v "count(/rss//item/title)" $file_mailxml)
-beitrag=1
+#Count entries in RSS
+counter_entries_in_rss=$(xmlstarlet sel -t -v "count(/rss//item/title)" $file_mailxml)
 
 #Import entries
-while [ $beitrag -le $beitragscounter ];do
-#Extract ID
-id=$(xmlstarlet sel -t -m "/rss//item[$beitrag]/guid" -v . -n $file_mailxml)
-#Check if ID already imported. Import only new entries
-if [ "$(grep "$id" $file_ids)" = "" ]; then
-#echo "New entry found!"
+while [ $counter_entry -le $counter_entries_in_rss ];do
+  #Extract ID
+  id=$(xmlstarlet sel -t -m "/rss//item[$counter_entry]/guid" -v . -n $file_mailxml)
+  #Check if ID already imported. Import only new entries
+  if [ "$(grep "$id" $file_ids)" = "" ]; then
+    #echo "New entry found!"
 
-#Titel
-entry_text=$(xmlstarlet sel -t -m "/rss//item[$beitrag]/title" -v . -n $file_mailxml)
-#echo "Entry imported: $entry_text"
+    #Titel
+    entry_text=$(xmlstarlet sel -t -m "/rss//item[$counter_entry]/title" -v . -n $file_mailxml)
 
-#URL
-url=$(xmlstarlet sel -t -m "/rss//item[$beitrag]/link" -v . -n $file_mailxml)
-#echo "URL: $url"
-#echo $(xmlstarlet sel -t -m "/rss//item[1]/description" -v . -n mail.xml | cut -d\; -f1)
-#echo $(xmlstarlet sel -t -m "/rss//item[1]/description" -v . -n mail.xml | cut -d' ' -f2)
-#echo $(xmlstarlet sel -t -m "/rss//item[1]/description" -v . -n mail.xml | cut -d' ' -f3)
-#echo $(xmlstarlet sel -t -m "/rss//item[1]/description" -v . -n mail.xml | cut -d' ' -f4)
+    #URL
+    url=$(xmlstarlet sel -t -m "/rss//item[$counter_entry]/link" -v . -n $file_mailxml)
+
+    #Store ID
+    echo $id >> $file_ids
 
 
-#echo $(xmlstarlet sel -t -m "/rss//item[$beitrag]/title" -v . -n mail.xml)
-#echo $(xml_grep --cond "/rss//item[$beitrag]/title" mail.xml)
-#echo "beitrag generiert"
-
-#Store ID
-echo $id >> $file_ids
+    #Create entry html
+    . $install_path/beitraggenerieren.sh
 
 
-#Create entry html
-. $install_path/beitraggenerieren.sh
-#else
-#	echo "Entry already imported!"
-fi
+    counter_entries_imported=counter_entries_imported+1
+  fi
 
-
-beitrag=$(($beitrag+1))
+  counter_entry=$(($counter_entry+1))
 done
 
 #Delete mail.xml
 rm $file_mailxml
 
+if [ $counter_entries_imported -ne 0 ]; then
+  #create new index.html
+  . $install_path/generate_index.sh
+fi
 
 
-#create new index.html
-. $install_path/generate_index.sh
-echo "Imported $beitrag entries."
+echo "Imported entries: $counter_entries_imported"
 echo "Email import done."
 
 echo "---------------------------------------------------"
