2578b8351f12bc774e0d29317a49712ef81dfe92
Marvin <marvin@vps.servercheap.net>
2017-11-14 23:06:24 +0100

    Initial install script. Creates directories.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+
+#Check for Config-File
+if [ ! -f ./config/config.conf ]; then
+   printf 'Config File not found!\nPlease copy './config/config_template.conf' to './config/config.conf' and fill in the 2 first paths.'
+   exit
+fi
+
+#Create File-Structure
+source ./config/config.conf
+
+folder=("$html_path/html/entries" "$html_path/html/entries" "$html_path/html/tags" "$html_path/html/pages" "$install_path/temp" "$html_path/config/" "$html_path/config/menu" "$html_path/config/menu/links")
+for DIRECTORY in "${folder[@]}"; do
+  mkdir $DIRECTORY
+done
+
+
